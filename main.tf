6f9c4ea052bb80a10c6a6625c2c3344806ded86d
AtharvShinde <atharvdshinde@gmail.com>
2025-01-11 14:31:21 +0530

    main.tf edit

--- main.tf
+++ main.tf
@@ -2,11 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create an S3 bucket
 resource "aws_s3_bucket" "data_bucket" {
   bucket = "s3-to-rds-glue-bucket"
 }
 
-resource "aws_rds_instance" "my_rds" {
+# Create an RDS MySQL instance
+resource "aws_db_instance" "my_rds" {
   allocated_storage    = 20
   engine               = "mysql"
   instance_class       = "db.t2.micro"
@@ -14,8 +16,27 @@ resource "aws_rds_instance" "my_rds" {
   username             = "admin"
   password             = "password123"
   publicly_accessible  = true
+  skip_final_snapshot  = true
 }
 
+# Create an IAM Role for Glue job
+resource "aws_iam_role" "glue_role" {
+  name = "glue-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Create a Glue job
 resource "aws_glue_job" "my_glue" {
   name       = "my-glue-job"
   role_arn   = aws_iam_role.glue_role.arn
@@ -25,7 +46,28 @@ resource "aws_glue_job" "my_glue" {
   }
 }
 
+# Create the Lambda function
 resource "aws_lambda_function" "s3_to_rds_glue_lambda" {
   function_name = "s3-to-rds-glue-lambda"
   image_uri     = "010928211649.dkr.ecr.us-east-1.amazonaws.com/s3-to-rds-glue:latest"
+  
+  # IAM Role for Lambda function
+  role = aws_iam_role.lambda_exec_role.arn
+}
+
+# Create IAM Role for Lambda Execution
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
